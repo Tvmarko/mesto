1aa48e43747e082122877aa8e98096a61f98999e
Tatyana Markova <tvmarko@yandex.ru>
2022-02-06 02:42:33 +0400

    5Pr1iter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,26 +4,35 @@ const openPopupButtonEdit = document.querySelector('.profile__edit-button')
 const openPopupButtonAdd = document.querySelector('.profile__add-button')
 const closePopupButtonEdit = document.querySelector('.popup__close-button_edit')
 const closePopupButtonAdd = document.querySelector('.popup__close-button_add')
-let formElement = document.querySelector('.popup__form')
-let profileName = document.querySelector('.profile__info-title')
-let profileJob = document.querySelector('.profile__info-subtitle')
-let nameInput = document.getElementById('name')
-let jobInput = document.getElementById('about')
+const closePhotoPopupButton = document.querySelector('.popup__close-button_photo')
+const formElement = document.querySelector('.popup__form')
+const profileName = document.querySelector('.profile__info-title')
+const profileJob = document.querySelector('.profile__info-subtitle')
+const nameInput = document.getElementById('name')
+const jobInput = document.getElementById('about')
+const template = document.querySelector('.elements-template').content
+const cards = document.querySelector('.elements')
+const formElementAdd = document.querySelector('.popup__form_add')
+const placeInput = document.getElementById('place')
+const imageInput = document.getElementById('image')
+const openPhotoPopup = document.querySelector('.popup_openImage')
+const openPhoto = document.querySelector('.popup__openphoto')
+const openPhotoCaption = document.querySelector('.popup__openphoto-caption')
 
 // функция для открытия попапа
 function openPopup (popup) {
   popup.classList.add('popup_opened')
 }
 
-// открываем попап редактирования
+// открытие попапа редактирования
 openPopupButtonEdit.addEventListener('click', function () {
   openPopup(popupEdit); 
-  // заносим данные в форму из профайла
+  // занесение данных в форму из профайла
   nameInput.value = profileName.textContent 
   jobInput.value = profileJob.textContent
 });
 
-// открываем попап добавления
+// открытие попапа добавления
 openPopupButtonAdd.addEventListener('click', function () {
   openPopup(popupAdd); 
 });
@@ -33,18 +42,23 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened')
 }
 
-// закрываем попап редактирования
+// закрытие попапа редактирования
 closePopupButtonEdit.addEventListener('click', function () {
   closePopup(popupEdit); 
 });
 
-// закрываем попап добавления
+// закрытие попапа добавления
 closePopupButtonAdd.addEventListener('click', function () {
   closePopup(popupAdd); 
 });
 
-// сохранить в профайле данные, занесенные в форму
-// закрыть попап при наступлении события 'submit'
+//закрытие попапа картинки
+closePhotoPopupButton.addEventListener('click', function () {
+  closePopup(openPhotoPopup); 
+});
+
+// сохранение в профайле данных, занесенных в форму
+// закрытие попапа при отправке 'submit'
 function formSubmitHandler(evt) {
     evt.preventDefault() 
     profileName.textContent = nameInput.value
@@ -82,23 +96,23 @@ const initialCards = [
     }
   ]; 
  
-  const template = document.querySelector('.elements-template').content
-  const cards = document.querySelector('.elements')
-  const formElementAdd = document.querySelector('.popup__form_add')
-  const placeInput = document.getElementById('place')
-  const imageInput = document.getElementById('image')
-   
   //создание карточки      
   function createItem(element) {
     const cardElement = template.querySelector('.elements__element').cloneNode(true)
-    const cardElementImg = cardElement.querySelector('.elements__element-image')
+    const cardElementImg = cardElement.querySelector('.elements__element-photo')
     cardElement.querySelector('.elements__element-title').textContent = element.name
     cardElementImg.src = element.link
     cardElementImg.alt = element.name
 
     cardElement.querySelector('.elements__element-like').addEventListener('click', function (evt) { 
-        evt.target.classList.toggle('elements__element-like_active')  
+      evt.target.classList.toggle('elements__element-like_active')  
     })
+
+    cardElement.querySelector('.elements__element-delete').addEventListener('click', function (evt) { 
+      evt.target.closest('.elements__element').remove()
+  })
+
+    cardElementImg.addEventListener('click', openPopupImage)
 
     return cardElement
   }
@@ -119,8 +133,20 @@ const initialCards = [
     
   // слушатель для отслеживания события 'submit'
   formElementAdd.addEventListener('submit', function (evt) {
+    evt.preventDefault()
     renderNewItem()
     closePopup(popupAdd)
+    evt.target.reset()
   })
+
+  //открытие попапа картинки
+  function openPopupImage(event) {
+    openPopup(openPhotoPopup)
+    openPhoto.src = event.target.src
+    openPhoto.alt = event.target.alt
+    openPhotoCaption.textContent = event.target.alt
+  }
+
+  
 
   
